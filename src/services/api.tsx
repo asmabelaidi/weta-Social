4100aa30bf2ef6c53ecd6f34f435d007017df7d7
Asma Belaidi <asma.belaidi40@gmail.com>
2023-07-20 12:22:48 -0700

    Add API_URL env variable

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -4,7 +4,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const userApi = createApi({
     reducerPath: 'userApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000',
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL || 'http://localhost:8000',
   }),
     
     endpoints: (builder) => ({
